d12a21939c0959abfb8ce9778272efe6429c097e
Elise Hoovler <elisejhoovler@live.com>
2021-01-16 02:38:12 -0600

    gatsby clean

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"Portfolio","short_name":"portfolio","start_url":"/","display":"minimal-ui","icon":"src/images/one.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"0a3750e6fdb29fcd632e627e2fc34dde"},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
